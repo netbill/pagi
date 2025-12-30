cab73b53406f9603cf20aa33071d516cf830f167
trpdjke <yehorushakov@gmail.com>
2025-12-30 23:15:17 +0200

    add GetPagination

--- http.go
+++ http.go
@@ -53,3 +53,32 @@ func GetSort(r *http.Request) *SortField {
 
 	return sort
 }
+
+func GetPagination(r *http.Request) (limit uint, offset uint) {
+	page := uint(1)
+	size := uint(20)
+
+	params := r.URL.Query()
+
+	if s := params.Get("page"); s != "" {
+		if n, err := strconv.ParseUint(s, 10, 32); err == nil && n > 0 {
+			page = uint(n)
+		}
+	}
+
+	if s := params.Get("size"); s != "" {
+		if n, err := strconv.ParseUint(s, 10, 32); err == nil && n > 0 {
+			size = uint(n)
+		}
+	}
+
+	const maxSize = uint(100)
+	if size > maxSize {
+		size = maxSize
+	}
+
+	offset = (page - 1) * size
+	limit = size
+
+	return
+}
